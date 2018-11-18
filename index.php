cb4739e157025539736465380c9de3927d8c6854
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-18 16:01:09 +0530

    added new features

--- index.php
+++ index.php
@@ -14,18 +14,20 @@ define( 'WPONION_FRONTEND', true );
 
 add_action( 'wponion_loaded', function () {
 	define( '_DIR_', __DIR__ . '/' );
-	global $wpof;
+
 	require_once _DIR_ . '/functions.php';
 	require_once _DIR_ . '/fields.php';
 	require_once _DIR_ . '/dashboard-widgets.php';
-	require_once _DIR_ . '/admin-page.php';
+	require_once _DIR_ . '/admin-columns.php';
+	require_once _DIR_ . '/quick-edit.php';
+	require_once _DIR_ . '/bulk-edit.php';
 	require_once _DIR_ . '/settings.php';
-	require_once __DIR__ . '/metabox.php';
+	require_once _DIR_ . '/admin-page.php';
 	require_once __DIR__ . '/user_profile.php';
+	require_once __DIR__ . '/metabox.php';
 	require_once __DIR__ . '/taxonomy.php';
 	require_once __DIR__ . '/help_tabs.php';
 	require_once __DIR__ . '/woocommerce.php';
-
 } );
 add_action( 'widgets_init', function () {
 	require_once __DIR__ . '/widgets.php';
