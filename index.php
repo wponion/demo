6d2a97c8f15425241ef55be1550e30348ed2d2f0
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-24 10:25:18 +0530

    updated

--- index.php
+++ index.php
@@ -13,21 +13,21 @@ define( 'WPONION_FRONTEND', true );
 
 
 add_action( 'wponion_loaded', function () {
-	define( '_DIR_', __DIR__ . '/' );
 
-	require_once _DIR_ . '/functions.php';
-	require_once _DIR_ . '/fields.php';
-	require_once _DIR_ . '/dashboard-widgets.php';
-	require_once _DIR_ . '/admin-columns.php';
-	require_once _DIR_ . '/quick-edit.php';
-	require_once _DIR_ . '/bulk-edit.php';
-	require_once _DIR_ . '/settings.php';
-	require_once _DIR_ . '/admin-page.php';
+	require_once __DIR__ . '/functions.php';
+	require_once __DIR__ . '/fields.php';
+	require_once __DIR__ . '/dashboard-widgets.php';
+	require_once __DIR__ . '/admin-columns.php';
+	require_once __DIR__ . '/quick-edit.php';
+	require_once __DIR__ . '/bulk-edit.php';
+	require_once __DIR__ . '/settings.php';
+	require_once __DIR__ . '/admin-page.php';
 	require_once __DIR__ . '/user_profile.php';
 	require_once __DIR__ . '/metabox.php';
 	require_once __DIR__ . '/taxonomy.php';
 	require_once __DIR__ . '/help_tabs.php';
 	require_once __DIR__ . '/woocommerce.php';
+	require_once __DIR__ . '/admin-bar.php';
 } );
 add_action( 'widgets_init', function () {
 	require_once __DIR__ . '/widgets.php';
