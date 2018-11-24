71b1cb47a9f4747186a093b87bb3317ce33700cf
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-24 10:23:36 +0530

    updated user profile with metabox layout & normal layout

--- user_profile.php
+++ user_profile.php
@@ -17,7 +17,44 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
-$instance = wponion_user_profile( array(
+wponion_user_profile( array(
+	'heading'     => __( 'Custom Profile Fields As Metabox' ),
+	'option_name' => '_wponion_user_profile_1',
+	'metabox'     => __( 'WPOnion User Profile Demo' ),
+), array(
+	array(
+		'title'  => 'Page 1',
+		'name'   => 'page1',
+		'fields' => array(
+			array(
+				'title' => 'Profile Field',
+				'type'  => 'text',
+				'id'    => 'profile',
+			),
+			array(
+				'title' => 'Profile Field 2',
+				'type'  => 'textarea',
+				'id'    => 'profile2',
+			),
+			array(
+				'id'      => 'image_select_checkbox',
+				'title'   => __( 'Image Select' ),
+				'desc'    => __( 'You can select multiple images' ),
+				'type'    => 'image_select',
+				'options' => array(
+					'image1' => 'https://s3.wponion.com/placeholder/75/1.jpg',
+					'image2' => 'https://s3.wponion.com/placeholder/75/2.jpg',
+					'image3' => 'https://s3.wponion.com/placeholder/75/3.jpg',
+					'image4' => 'https://s3.wponion.com/placeholder/75/4.jpg',
+					'image5' => 'https://s3.wponion.com/placeholder/75/5.jpg',
+				),
+			),
+		),
+	),
+) );
+
+wponion_user_profile( array(
+	'heading'     => __( 'Custom Profile Fields' ),
 	'option_name' => '_wponion_user_profile',
 ), array(
 	array(
@@ -44,7 +81,3 @@ $instance = wponion_user_profile( array(
 		),
 	),
 ) );
-
-if ( wponion_is_debug() ) {
-	wponion_user_profile( array( 'option_name' => '_wponion_user_profile_dev' ), wponion_demo_get_all_fields( 'all' ) );
-}
