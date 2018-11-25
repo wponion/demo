0650c29c6104694480642d4ed651018d7a5d9637
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-25 11:59:47 +0530

    Code Formatted

--- widgets.php
+++ widgets.php
@@ -13,27 +13,40 @@
  * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
-class WPOnion_Demo_Widget extends \WPOnion\WP_Widget {
-	public function __construct() {
-		parent::__construct( 'wponion-demo-widget', __( 'WPOnion Demo Widget' ) );
+if ( class_exists( '\WPOnion\WP_Widget' ) ) {
+	/**
+	 * Class WPOnion_Demo_Widget
+	 *
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class WPOnion_Demo_Widget extends \WPOnion\WP_Widget {
+		/**
+		 * WPOnion_Demo_Widget constructor.
+		 */
+		public function __construct() {
+			parent::__construct( 'wponion-demo-widget', __( 'WPOnion Demo Widget' ) );
+		}
+
+		/**
+		 * @return array
+		 */
+		public function fields() {
+			return array(
+				array(
+					'title'    => __( 'Text Field' ),
+					'type'     => 'text',
+					'id'       => 'text',
+					'validate' => 'wponion_is_url',
+				),
+				array(
+					'title' => __( 'Icon Picker' ),
+					'type'  => 'icon_picker',
+					'id'    => 'icon',
+				),
+			);
+		}
 	}
 
-	public function fields() {
-		return array(
-			array(
-				'title'    => __( 'Text Field' ),
-				'type'     => 'text',
-				'id'       => 'text',
-				'validate' => 'wponion_is_url',
-			),
-			array(
-				'title' => __( 'Icon Picker' ),
-				'type'  => 'icon_picker',
-				'id'    => 'icon',
-			),
-		);
-	}
+	register_widget( 'WPOnion_Demo_Widget' );
 }
-
-register_widget( 'WPOnion_Demo_Widget' );
-
