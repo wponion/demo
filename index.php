2b34d62df3a729d95a6f7c3ece4350404da47d45
Varun Sridharan <varunsridharan23@gmail.com>
2018-12-05 08:30:34 +0530

    Updated

--- index.php
+++ index.php
@@ -11,26 +11,29 @@
  */
 define( 'WPONION_FRONTEND', true );
 
-add_action( 'wponion_loaded', function () {
-
-	require_once __DIR__ . '/functions.php';
-	require_once __DIR__ . '/fields.php';
-	require_once __DIR__ . '/dashboard-widgets.php';
-	require_once __DIR__ . '/admin-columns.php';
-	require_once __DIR__ . '/quick-edit.php';
-	require_once __DIR__ . '/bulk-edit.php';
-	require_once __DIR__ . '/settings.php';
-	require_once __DIR__ . '/admin-page.php';
-	require_once __DIR__ . '/user_profile.php';
-	require_once __DIR__ . '/metabox.php';
-	require_once __DIR__ . '/taxonomy.php';
-	require_once __DIR__ . '/help_tabs.php';
-	require_once __DIR__ . '/woocommerce.php';
-	require_once __DIR__ . '/admin-bar.php';
-	require_once __DIR__ . '/nav_menu.php';
-	require_once __DIR__ . '/admin-notice.php';
-
-} );
+add_action( 'wponion_loaded', 'wponion_load_demo' );
 add_action( 'widgets_init', function () {
 	require_once __DIR__ . '/widgets.php';
 }, 10 );
+
+if ( ! function_exists( 'wponion_load_demo' ) ) {
+	function wponion_load_demo() {
+		require_once __DIR__ . '/functions.php';
+		require_once __DIR__ . '/fields.php';
+		require_once __DIR__ . '/dashboard-widgets.php';
+		require_once __DIR__ . '/admin-columns.php';
+		require_once __DIR__ . '/quick-edit.php';
+		require_once __DIR__ . '/bulk-edit.php';
+		require_once __DIR__ . '/settings.php';
+		require_once __DIR__ . '/admin-page.php';
+		require_once __DIR__ . '/user_profile.php';
+		require_once __DIR__ . '/metabox.php';
+		require_once __DIR__ . '/taxonomy.php';
+		require_once __DIR__ . '/help_tabs.php';
+		require_once __DIR__ . '/woocommerce.php';
+		require_once __DIR__ . '/admin-bar.php';
+		require_once __DIR__ . '/nav_menu.php';
+		require_once __DIR__ . '/admin-notice.php';
+		require_once __DIR__ . '/media-fields.php';
+	}
+}
