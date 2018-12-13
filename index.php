d38524d4be766906b6b3ad2a7f28a4d67e424711
Varun Sridharan <varunsridharan23@gmail.com>
2018-12-13 09:32:30 +0530

    updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPOnion Demo
  * Plugin URI: https://github.com/wponion/demo/
  * Description: Simple Demo Plugin For WPOnion
- * Version: 161120180115
+ * Version: 161120180116
  * Author: WPOnion,VarunSridharan
  * Author URI: https://github.com/wponion/
  * Text Domain: wponion
