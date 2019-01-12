0aa626e132cc2336f8018706f370429f1a8f05c7
Varun <varunsridharan23@gmail.com>
2019-01-12 13:26:53 +0530

    updated

--- admin-notice.php
+++ admin-notice.php
@@ -6,10 +6,17 @@
  * Time: 06:47 PM
  */
 
-if ( isset( $_REQUEST['page'] ) && $_REQUEST['page'] === 'custom-wponion-page' ) {
+if ( isset( $_REQUEST['page'] ) && 'custom-wponion-page' === $_REQUEST['page'] ) {
 	wponion_success_admin_notice( 'Success Notice', false, array( 'screens' => array( 'dashboard_page_custom-wponion-page' ) ) );
 	wponion_info_admin_notice( 'Info Notice', false, array( 'screens' => array( 'dashboard_page_custom-wponion-page' ) ) );
 	wponion_warning_admin_notice( 'Warning Notice', false, array( 'screens' => array( 'dashboard_page_custom-wponion-page' ) ) );
 	wponion_error_admin_notice( 'error Notice', false, array( 'screens' => array( 'dashboard_page_custom-wponion-page' ) ) );
+	wponion_error_admin_notice( 'error Notice', false, array(
+
+		'alt'     => true,
+		'screens' => array(
+			'dashboard_page_custom-wponion-page',
+		),
+	) );
 	wponion_upgrade_admin_notice( 'Upgrade Notice', false, array( 'screens' => array( 'dashboard_page_custom-wponion-page' ) ) );
 }
