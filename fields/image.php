63808da33a626bae0083a32513ee135e1f04985f
Varun <varunsridharan23@gmail.com>
2019-01-31 16:18:53 +0530

    Added Image Demo.

---
+++ fields/image.php
@@ -0,0 +1,9 @@
+<?php
+
+$return = array();
+
+$return[] = \WPO\Field::create( 'image', 'image', __( 'WP Image Selector' ) );
+$return[] = \WPO\Field::create( 'image', 'image_1', __( 'Custom Frame Title' ) )
+	->set_frame_title( 'Select A Background Image' );
+
+return $return;
