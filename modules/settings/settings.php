a6a1ba335e157a86a04f9cedbb95e3f729ad682d
Varun <varunsridharan23@gmail.com>
2019-02-01 13:33:07 +0530

    Settings Module Mostly Done.

--- modules/settings/settings.php
+++ modules/settings/settings.php
@@ -7,6 +7,32 @@ foreach ( $wpo as $container ) {
 	$builder->container( $container );
 }
 
+$builder->container( 'system-info', __( 'System Tool/Info' ), 'dashicons dashicons-info' )
+	->set_callback( 'wponion_sysinfo' );
+
+
+$builder->separator( 'separator', 'Custom Seperator', 'dashicons dashicons-admin-generic' );
+
+$builder->container( 'custom_page', __( 'Custom Page' ) )
+	->set_callback( function () {
+		echo '<div class="col-xs-12"><h1>Hello World. This is a custom page with custom content rendered</h1></div>';
+	} );
+
+$builder->separator( 'separator2' );
+
+$subpages = $builder->container( 'custom_subpages', __( 'Custom Subpages' ) );
+$subpages->container( 'page1', __( 'Page 1' ) )
+	->set_callback( function () {
+		echo '<div class="col-xs-12"><h1>Hello World. This is a custom page with custom content rendered</h1></div>';
+	} );
+$subpages->container( 'page2', __( 'Page 2' ) )
+	->set_callback( function () {
+		echo '<div class="col-xs-12"><h1>Hello World. This is a custom page with custom content rendered</h1></div>';
+	} );
+
+
+$builder->container( 'support', __( 'Support' ), 'dashicons-info dashicons' )
+	->set_href( 'https://github.com/wponion/' );
 $args = array(
 	'option_name'     => '_wponion_demo_settings',
 	'framework_title' => __( 'WPOnion Demo Framework' ),
