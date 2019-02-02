a2cabd28868b0ddcce20ab37184aef0ffaf1a21b
Varun <varunsridharan23@gmail.com>
2019-02-02 09:44:12 +0530

    Added Javascript validation

--- fields/js-validation.php
+++ fields/js-validation.php
@@ -6,6 +6,17 @@ $return[] = WPO\Field::create( 'text', 'text', __( 'Required Textfield *' ) )
 	->set_js_validate( 'required' );
 
 $return[] = WPO\Field::create( 'textarea', 'textarea', __( 'Textarea' ) )
+	->set_js_validate( array(
+		'minlength' => 10,
+		'maxlength' => 300,
+	) )
+	->set_desc_field( 'Minium Length : 10 & Max Length : 300' );
+$return[] = WPO\Field::create( 'checkbox', 'checkbox', __( 'Required Checkbox *' ) )
+	->set_js_validate( 'required' );
+$return[] = WPO\Field::create( 'switcher', 'switcher', __( 'Required Switcher *' ) )
+	->set_js_validate( 'required' );
+
+$return[] = WPO\Field::create( 'key_value', 'key_value', __( 'Required Key Value *' ) )
 	->set_js_validate( 'required' );
 
 return $return;
