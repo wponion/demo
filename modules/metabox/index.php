f321fd86ccd7871af1ee14a4e0cf688e88410fd2
Varun <varunsridharan23@gmail.com>
2020-01-18 07:06:14 +0530

    Minor metabox bug fixed.

--- modules/metabox/index.php
+++ modules/metabox/index.php
@@ -1,4 +1,4 @@
 <?php
-require_once __DIR__ . './simple-metabox.php';
-require_once __DIR__ . './simple-side.php';
-require_once __DIR__ . './main-metabox.php';
+require_once __DIR__ . '/simple-metabox.php';
+require_once __DIR__ . '/simple-side.php';
+require_once __DIR__ . '/main-metabox.php';
