d175c269c3cb57c077a451d45ca294b84fda9f4b
Varun <varunsridharan23@gmail.com>
2020-03-22 19:08:23 +0530

    Updated Dependency demo

--- fields/dependency.php
+++ fields/dependency.php
@@ -1,29 +1,43 @@
 <?php
-$return[] = WPO\Field::create( 'text', 'field1', '== Condition' )
-	->desc_field( 'Type <code>Hello</code> in the field above.' );
-$return[] = WPO\Field::create( 'notice_success', __( 'Awesome You Found The Hidden Field !!' ) )
-	->dependency( 'field1', '=', 'Hello' );
+$return = array();
 
-$return[] = WPO\Field::create( 'subheading', '(Any) Condition' );
-
-$return[] = WPO\Field::create( 'text', 'field2', 'Field 2' )
-	->desc_field( 'Type <code>Bumble</code>  OR <code>Jack</code> in the field above.' );
-$return[] = WPO\Field::create( 'notice_success', __( 'Awesome You have enterd correct word (Bumble or Jack)' ) )
-	->dependency( 'field2', 'any', 'Bumble,Jack' );
+$return[]  = wpo_field( 'subheading', __( 'Checkbox Dependency' ) );
+$return[]  = wpo_field( 'notice', __( 'Text field will be enabled and revealed when the below checkbox is checked.' ) );
+$return[]  = wpo_field( 'checkbox', 'checkbox_1', __( 'Check me ' ) )->label( __( 'Check To Enable A Text Field' ) );
+$textfield = wpo_field( 'text', 'text_1', __( 'Textfield' ) );
+$textfield->dependency( 'checkbox_1', 'checked', 'true' );
+$return[] = $textfield;
 
 
-$return[] = WPO\Field::create( 'subheading', '(OR) Condition' );
-$return[] = WPO\Field::create( 'checkbox', 'checkbox_1', 'Checkbox 1' );
-$return[] = WPO\Field::create( 'checkbox', 'checkbox_2', 'Checkbox 2' );
+$return[]     = wpo_field( 'subheading', __( 'Word Dependency' ) );
+$return[]     = wpo_field( 'notice', __( 'A Notice will be revealed when the text field value is either <code>yes</code> or <code>true</code>>' ) );
+$return[]     = wpo_field( 'text', 'text_2', __( 'Show Notice ?' ) );
+$notice_field = wpo_field( 'notice', __( 'You Have Successfully Found The Hidden Notice' ) )->notice_type( 'warning' );
+$notice_field->dependency( 'text_2', 'values', array( 'yes', 'true' ) );
+$return[] = $notice_field;
 
-$return[] = WPO\Field::create( 'notice_success', __( 'You Either Checked (checkbox1) or (checkbox2)' ) )
-	->dependency( 'checkbox_1', 'OR', '1' )
-	->dependency( 'checkbox_2', 'OR', '1' );
+$return[]     = wpo_field( 'subheading', __( 'Multiple Dependencies' ) );
+$return[]     = wpo_field( 'notice', __( 'A Notice will be revealed when the checkbox is checked and the text field value is an email address' ) );
+$return[]     = wpo_field( 'checkbox', 'checkbox_2', __( 'Send Emails ? ' ) );
+$return[]     = wpo_field( 'text', 'text_3', __( 'Your Email ID' ) );
+$notice_field = wpo_field( 'notice', __( 'You Have Successfully Found The Hidden Notice' ) )->notice_type( 'warning' );
+$notice_field->dependency( 'checkbox_2', 'checked', 'true' )
+	->rule( 'text_3', 'email', true );
+$return[] = $notice_field;
 
-$return[] = WPO\Field::create( 'subheading', '(!==) Condition' );
-$return[] = WPO\Field::create( 'text', 'field3', '!= Condition' )
-	->field_default( 'WordPress' )
-	->desc_field( 'Type Any Other Word Than <code>WordPress</code> in the field above.' );
-$return[] = WPO\Field::create( 'notice_success', __( 'Awesome You Found The Hidden Field !!' ) )
-	->dependency( 'field3', '!=', 'WordPress' );
+$return[]  = wpo_field( 'subheading', __( 'Modifying Field Value' ) );
+$return[]  = wpo_field( 'notice', __( 'Change the text field value when the select menu changes.' ) );
+$return[]  = wpo_field( 'select', 'select_1', __( 'Enable ? ' ) )
+	->option( 'Yes', 'Yes' )
+	->option( 'No', 'No' );
+$textfield = wpo_field( 'text', 'text_4', __( 'Your Message' ) );
+$textfield->dependency( 'select_1', 'values', array( 'Yes' ) );
+$textfield->dependency_settings( array(
+	'valueOnEnable'  => 'Hello, world!',
+	'valueOnDisable' => 'Goodbye, cruel world...',
+	'hide'           => false,
+) );
+$return[] = $textfield;
+
+
 return $return;
