cf0903929ec278dc475923d07a59bf5b0c0a5cd9
Varun <varunsridharan23@gmail.com>
2020-05-06 07:46:30 +0530

    Updated column class.

--- fields/input-group.php
+++ fields/input-group.php
@@ -2,22 +2,22 @@
 $return = array();
 
 $group = \WPO\Field::create( 'input_group', 'input_group_1', __( 'Address Field (Input Group)' ) );
-$group->text( 'first_name', __( 'First Name' ), array( 'wrap_class' => 'col-xs-12 col-sm-12 col-md-6' ) );
-$group->text( 'last_name', __( 'Last Name' ), array( 'wrap_class' => 'col-xs-12 col-sm-12 col-md-6' ) );
-$group->textarea( 'address', __( 'House Address' ), array( 'wrap_class' => 'col-xs-12 col-sm-12 col-md-6' ) )
+$group->text( 'first_name', __( 'First Name' ), array( 'wrap_class' => 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-md-6' ) );
+$group->text( 'last_name', __( 'Last Name' ), array( 'wrap_class' => 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-md-6' ) );
+$group->textarea( 'address', __( 'House Address' ), array( 'wrap_class' => 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-md-6' ) )
 	->horizontal( true );
-$group->textarea( 'address_office', __( 'Office Address' ), array( 'wrap_class' => 'col-xs-12 col-sm-12 col-md-6' ) )
+$group->textarea( 'address_office', __( 'Office Address' ), array( 'wrap_class' => 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-md-6' ) )
 	->horizontal( true );
 $return[] = $group;
 
 $group2 = \WPO\Field::create( 'input_group', 'input_group_2', __( 'Address Field (Input Group)' ) );
 $group2->text( 'textfield', __( 'Textfield' ) )
 	->horizontal( true )
-	->wrap_class( 'col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4' );
+	->wrap_class( 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-md-6 wpo-col-lg-6 wpo-col-xl-4' );
 $group2->checkbox( 'checkbox', __( 'Checkbox' ) )
 	->label( 'I Agree.' )
 	->horizontal( true )
-	->wrap_class( 'col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4' );
+	->wrap_class( 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-md-6 wpo-col-lg-6 wpo-col-xl-4' );
 $group2->select( 'select', __( 'Select' ), array(
 	'options' => array(
 		'option1' => __( 'Option 1' ),
@@ -27,7 +27,7 @@ $group2->select( 'select', __( 'Select' ), array(
 	),
 ) )
 	->horizontal( true )
-	->wrap_class( 'col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4' );
+	->wrap_class( 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-md-6 wpo-col-lg-6 wpo-col-xl-4' );
 $return[] = $group2;
 
 return $return;
