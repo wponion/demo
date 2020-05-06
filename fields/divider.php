ea947bd31d121e6e5740fe4953ee36e4783f65c8
Varun <varunsridharan23@gmail.com>
2020-05-06 07:46:08 +0530

    Updated column class.

--- fields/divider.php
+++ fields/divider.php
@@ -8,10 +8,10 @@ $return[] = WPO\Field::create( 'text', 'yournumber', __( 'Your Mobile No' ) );
 $return[] = WPO\Field::create( 'text', 'yoremail', __( 'Your Email' ) );
 $return[] = WPO\Field::create( 'notice_info', 'Divider Can also have Custom Text' );
 
-$return[] = WPO\Field::create( 'text', 'yourname2', __( 'Your Name' ), array( 'wrap_class' => 'col-xs-12 col-sm-12 col-lg-6' ) );
-$return[] = WPO\Field::create( 'textarea', 'youraddress2', __( 'Your Address' ), array( 'wrap_class' => 'col-xs-12 col-sm-12 col-lg-6' ) );
+$return[] = WPO\Field::create( 'text', 'yourname2', __( 'Your Name' ), array( 'wrap_class' => 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-lg-6' ) );
+$return[] = WPO\Field::create( 'textarea', 'youraddress2', __( 'Your Address' ), array( 'wrap_class' => 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-lg-6' ) );
 $return[] = WPO\Field::create( 'divider', 'OR' );
-$return[] = WPO\Field::create( 'text', 'yournumber2', __( 'Your Mobile No' ), array( 'wrap_class' => 'col-xs-12 col-sm-12 col-lg-6' ) );
-$return[] = WPO\Field::create( 'text', 'yoremail2', __( 'Your Email' ), array( 'wrap_class' => 'col-xs-12 col-sm-12 col-lg-6' ) );
+$return[] = WPO\Field::create( 'text', 'yournumber2', __( 'Your Mobile No' ), array( 'wrap_class' => 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-lg-6' ) );
+$return[] = WPO\Field::create( 'text', 'yoremail2', __( 'Your Email' ), array( 'wrap_class' => 'wpo-col-xs-12 wpo-col-sm-12 wpo-col-lg-6' ) );
 
 return $return;
